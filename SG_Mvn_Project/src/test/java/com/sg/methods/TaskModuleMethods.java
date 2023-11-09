6109bbdbde58348a5740d72daf5a31dd517c1178
sgTester <gopal.gudi@gmail.com>
2023-11-09 07:49:17 +0530

    remove the method to TaskModuleMethods.java

--- SG_Mvn_Project/src/test/java/com/sg/methods/TaskModuleMethods.java
+++ SG_Mvn_Project/src/test/java/com/sg/methods/TaskModuleMethods.java
@@ -3,7 +3,4 @@ package com.sg.methods;
 import com.sg.driver.DriverScript;
 
 public class TaskModuleMethods extends DriverScript{
-    public void test(){
-        System.out.println("Sample code");
-    }
 }
